08dd8ded03c24873e2a53be0d7ef3cccedb79c77
Tukur <96329892+Tukur-Sunusi-gama@users.noreply.github.com>
2023-08-04 02:33:24 +0100

    Initial commit

---
+++ MyFirstWebApp/Controllers/ComicBookGalleryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class ComicBookGalleryController: Controller
+    {
+        public ContentResult Detail()
+        {
+            return new ContentResult()
+            {
+                Content = "hello world"
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MyFirstWebApp
+ New MVC
